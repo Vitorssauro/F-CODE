a32b5d4b99e3145840c6efca1bbfaaed19dc6365
Vitorssauro <vitorvtr2003@egmail.com>
2023-06-11 01:21:33 -0300

    11/06/2023

---
+++ Projeto1Semestre/script.js
@@ -0,0 +1,11 @@
+function Cadastrar() {
+
+    let Nome = document.getElementById('MyCad').pnome.value;
+    
+    if (Nome == "") {
+        alert("Insira seu Nome.");
+
+    } else {
+        alert("aeew");
+    }
+}
