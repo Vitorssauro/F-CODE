b5dc6469d501c95ce1b860dffa690567ff368337
Vitorssauro <vitorvtr2003@egmail.com>
2023-06-11 23:59:19 -0300

    11/06/2023

--- Projeto1Semestre/script.js
+++ Projeto1Semestre/script.js
@@ -1,11 +1,32 @@
 function Cadastrar() {
 
     let Nome = document.getElementById('MyCad').pnome.value;
-    
+    let Mail = document.getElementById('MyCad').pmail.value;
+    let ConMail = document.getElementById('MyCad').pconmail.value;
+    let Senha = document.getElementById('MyCad').psenha.value;
+    let ConSenha = document.getElementById('MyCad').pconsenha.value;
+
     if (Nome == "") {
         alert("Insira seu Nome.");
-
-    } else {
-        alert("aeew");
-    }
+    } else
+        if (Mail == "") {
+            alert("Insira seu E-Mail.");
+        } else
+            if (ConMail == "") {
+                alert("Confirme seu E-mail.");
+            } else
+                if (Mail != ConMail) {
+                    alert("E-mails diferentes! Informe novamente.");
+                } else
+                    if (Senha == "") {
+                        alert("Insira sua Senha.");
+                    } else
+                        if (ConSenha == "") {
+                            alert("Confrime sua Senha.");
+                        } else
+                            if (Senha != ConSenha) {
+                                alert("Senhas diferentes! Informe novamente.");
+                            } else {
+                                alert("Cadastro feito com Sucesso!!")
+                            }
 }
